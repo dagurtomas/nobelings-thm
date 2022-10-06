705f06a4704861baa2e0e57d95c508ad20442932
dagurtomas <dagurtomas@gmail.com>
2022-10-06 15:23:39 +0200

    add characterisation of connected topological spaces

--- src/nobeling.lean
+++ src/nobeling.lean
@@ -13,6 +13,7 @@ import algebra.category.Group.biproducts
 import algebra.category.Group.abelian
 import algebra.category.Group.Z_Module_equivalence
 import category_theory.category.ulift
+import control.ulift 
 
 -- set_option pp.universes true
 
@@ -219,6 +220,191 @@ def subgroup_of_continuous_maps (X : Top.{u}) (A : Ab.{u}) : add_subgroup (X →
   } 
 
 def ab_u_Z : Ab.{u} := ⟨ulift.{u} ℤ⟩
+
+def ring_u_Z : Ring.{u} := ⟨ulift.{u} ℤ⟩
+
+def CSZ (S : Profinite.{u}) : Ab.{u} := ⟨subgroup_of_continuous_maps (Profinite.to_Top.obj S) ab_u_Z⟩
+
+def zeroone_subset : set ℤ := {0, 1}
+
+def zeroone' : Top.{u} := Top.discrete.obj (ulift.{u} zeroone_subset) 
+
+def zeroone : Top.{u} := Top.discrete.obj (ulift.{u} {n : ℤ | 0 ≤ n ∧ n ≤ 1}) --don't use
+
+def zeroone_map (I : Type u) : I → Top.{u} := λ i, zeroone'
+
+def prod01_aslimit (I : Type u) := Top.pi_fan.{u u} (zeroone_map I)
+
+def prod01 (I : Type u) : Top.{u} := (prod01_aslimit I).X
+
+def prod_proj {I : Type u} (i : I) : prod01 I ⟶ zeroone' :=
+begin 
+  let p := Top.pi_π.{u u} (zeroone_map I) i,
+  have hz : zeroone' = zeroone_map I i,
+  refl,
+  rw hz,
+  have hp : prod01 I = Top.of (Π (i : I), ↥(zeroone_map I i)),
+  exact Top.pi_fan_X (zeroone_map I),
+  rw hp,
+  exact p,
+end
+
+lemma inj_to_prod (S : Profinite.{u}) : ∃ (I : Type u), ∃ (f : (Profinite.to_Top.obj S) ⟶ (prod01 I)), function.injective f := sorry 
+
+def I' (S : Profinite.{u}) := (inj_to_prod S).some
+
+-- def inj_to_prod_map (S : Profinite.{u}) : (Profinite.to_Top.obj S) ⟶ (prod01 (I S)) := {
+
+-- },
+
+def lambda (S : Profinite.{u}) := ordinal.type (@well_ordering_rel (I' S))
+
+-- lemma inj_to_prod_map_existence (S : Profinite.{u}) : ∃ (f : (Profinite.to_Top.obj S) ⟶ (prod01 (I' S))), function.injective f := 
+-- begin
+--   have h : ∃ (I : Type u), ∃ (f : (Profinite.to_Top.obj S) ⟶ (prod01 I)), function.injective f,
+--   exact inj_to_prod S,
+--   obtain ⟨I, f, h1⟩ := h,
+--   sorry,
+--   -- have h : I = (I' S),
+-- end
+
+-- instance CSZ_ab_grp (S : Profinite.{u}) : add_comm_group (CSZ S) := by apply_instance
+
+-- def ECSZ {S : Profinite.{u}} {I : Type u} (f : (Profinite.to_Top.obj S) ⟶ (prod01 I)) (i : I) : CSZ S :=
+-- begin 
+--   let pi := prod_proj i,
+--   let inj : {n : ℤ | 0 ≤ n ∧ n ≤ 1} → ℤ := λ x, x,
+--   let uinj : (ulift.{u} {n : ℤ | 0 ≤ n ∧ n ≤ 1}) → (ulift.{u} ℤ) := ulift.map inj,
+--   let ui := Top.discrete.map uinj,
+--   let elt : (Profinite.to_Top.obj S ⟶ (Top.discrete.obj ab_u_Z)) := f ≫ pi ≫ ui,
+--   have h : (CSZ S).α = subgroup_of_continuous_maps (Profinite.to_Top.obj S) ab_u_Z := by refl,
+--   unfold_coes,
+--   rw h,
+--   unfold_coes,
+--   use elt,
+--   unfold subgroup_of_continuous_maps,
+--   have helt : ⇑elt = (incl_of_continuous_maps (Profinite.to_Top.obj S) (ab_u_Z)) elt := by refl,
+--   rw helt,
+--   suffices hh : incl_of_continuous_maps (Profinite.to_Top.obj S) ab_u_Z elt ∈ set.range (incl_of_continuous_maps (Profinite.to_Top.obj S) ab_u_Z),
+--   exact hh,
+--   tidy, 
+-- end
+
+def EC_as_map {S : Profinite.{u}} {I : Type u} (f : (Profinite.to_Top.obj S) ⟶ (prod01 I)) (i : I) :
+  (Profinite.to_Top.obj S) → ring_u_Z :=
+begin
+  let pi : (prod01 I) → zeroone' := prod_proj i,
+  let ff : (Profinite.to_Top.obj S) → (prod01 I) := f,
+  let incl : zeroone_subset → ℤ := λ x, x,
+  let uincl : zeroone' → ulift.{u} ℤ := ulift.map incl,
+  -- let inj : {n : ℤ | 0 ≤ n ∧ n ≤ 1} → ℤ := λ x, x,
+  -- let uinj : (ulift.{u} {n : ℤ | 0 ≤ n ∧ n ≤ 1}) → (ulift.{u} ℤ) := ulift.map inj,
+  -- let ui := Top.discrete.map uinj,
+  exact uincl ∘ pi ∘ ff,
+  -- exact f ≫ pi ≫ ui,
+end
+
+lemma e_idempotent (S : Profinite.{u}) (I : Type u) : ∀ (i : I) (f : (Profinite.to_Top.obj S) ⟶ (prod01 I)) (s : (Profinite.to_Top.obj S)), 
+  ((EC_as_map f i) s) * ((EC_as_map f i) s) = ((EC_as_map f i) s) :=
+begin
+  intros i f s,
+  let ff : (Profinite.to_Top.obj S) → (prod01 I) := f,
+  let pi : (prod01 I) → zeroone' := prod_proj i,
+  let incl : zeroone_subset → ℤ := λ x, x,
+  let uincl : zeroone' → ulift.{u} ℤ := ulift.map incl,
+  -- let inj : {n : ℤ | 0 ≤ n ∧ n ≤ 1} → ℤ := λ x, x,
+  -- let uinj : (ulift.{u} {n : ℤ | 0 ≤ n ∧ n ≤ 1}) → (ulift.{u} ℤ) := ulift.map inj,
+  -- let ui := Top.discrete.map uinj,
+  have hE : (EC_as_map f i) s = uincl (pi (ff s)) := by refl,
+  have hzero : zeroone_subset = {0, 1} := by refl,
+  have h01 : ∀ n : ℤ, n ∈ zeroone_subset → n = 1 ∨ n = 0,
+  { intros n hn,
+    exact or.swap hn,
+  },
+  have hincl1 : ∀ x, incl x ∈ zeroone_subset,
+  { intros x,
+    exact subtype.mem x,
+  },
+  have hincl : ∀ x, incl x = 1 ∨ incl x = 0,
+  { intros x,
+    -- rw hzero at x,
+    apply h01 (incl x),
+    exact hincl1 x,
+  },
+  have h : (EC_as_map f i) s = 1 ∨ (EC_as_map f i) s = 0,
+  { rw hE,
+    specialize hincl (ulift.down (pi (ff s))),
+    cases hincl,
+    left,
+    apply_fun ulift.down,
+    exact hincl,
+    exact ulift.down_injective,
+    right,
+    apply_fun ulift.down,
+    exact hincl,
+    exact ulift.down_injective,
+  },
+  cases h,
+  rw h,
+  refl,
+  rw h,
+  refl,
+end
+
+def forget_ring : Ring.{u} ⥤ Ab.{u} :=
+{
+  obj := λ R, ⟨R⟩,
+  map := 
+  begin
+    intros R S f,
+    fconstructor,
+    intros r,
+    exact f r,
+    simp at *,
+    intros x y,
+    tidy,
+  end,
+  map_id' := by tidy,
+  map_comp' := by tidy,
+}
+
+-- def I_cst_map (I : Type u) : ℕ → Type u := λ n, I 
+
+def prod_N (I : Type u) : Type u := Π (n : ℕ), I  
+
+def N_order : (ℕ → ℕ → Prop) := λ n m, n ≤ m
+
+def prod_order (I : Type u) : Π (n : ℕ), I → I → Prop := λ n, @well_ordering_rel I 
+
+def lex_order (I : Type u) : (prod_N I) → (prod_N I) → Prop := λ x y, pi.lex N_order (prod_order I) x y 
+
+def sub_prod (I : Type u) : Type u := 
+  {x : prod_N I // (∀ n : ℕ, ordinal.typein (well_ordering_rel) (x n) ≠ 0 → (ordinal.typein (well_ordering_rel) (x n) > ordinal.typein (well_ordering_rel) (x n.succ))) ∧ 
+    (∃ n : ℕ, ∀ m ≥ n, ordinal.typein (well_ordering_rel) (x m) = 0)}
+
+def subset_prod (I : Type u) : set (prod_N I) := 
+  {x : prod_N I | (∀ n : ℕ, ordinal.typein (well_ordering_rel) (x n) ≠ 0 → (ordinal.typein (well_ordering_rel) (x n) > ordinal.typein (well_ordering_rel) (x n.succ))) ∧ 
+    (∃ n : ℕ, ∀ m ≥ n, ordinal.typein (well_ordering_rel) (x m) = 0)}
+
+def Eset (S : Profinite.{u}) (I : Type u) : set (prod_N I) :=
+begin
+  intros x,
+
+end
+
+-- variables I : Type u
+-- variables (x y : sub_prod I)
+-- #check lex_order I x y
+
+
+-- noncomputable def to_prod_map (S : Profinite.{u}) := (inj_to_prod_map_existence S).some 
+
+-- variables I : Type u
+-- variables i j : I
+-- #check @well_ordering_rel I 
+-- #check Well_order 
+-- #check ordinal.type (@well_ordering_rel I)
+-- The type I'm checking above is I as an ordinal (by the well-ordering theorem). 
 
 theorem CSZ_is_free (S : Profinite.{u}) : ∃ (I : Type u) (f : (AddCommGroup.of (subgroup_of_continuous_maps (Profinite.to_Top.obj S) ab_u_Z)) ⟶ AddCommGroup.of (free_abelian_group I)), is_iso f := 
 begin
