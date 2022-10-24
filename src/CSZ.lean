857f02076203ef3ff750fb38a6b3e6c4090e2954
dagurtomas <dagurtomas@gmail.com>
2022-10-24 13:40:03 +0000

    state nobeling's theorem in terms of algebras

--- src/CSZ.lean
+++ src/CSZ.lean
@@ -1,4 +1,49 @@
+import topology.category.Profinite.default
 import injective_map
+import algebra.free_algebra
 
-variables (S : Type*) [topological_space S] [compact_space S] [t2_space S] [totally_disconnected_space S]
+universe u
 
+instance totally_separated_of_profinite (S : Profinite.{u}) : totally_separated_space S :=
+  totally_separated_of_totally_disconnected_compact_hausdorff S
+  
+def I (S : Profinite.{u}) : Type u := 
+  {fI : S → bool // continuous fI}
+
+def inj_map (S : Profinite.{u}) : S → ((I S) → bool) :=
+  map_to_I' S
+
+def CSZ (S : Profinite.{u}) : Type u := {f : S → ℤ // continuous f}
+
+instance ring_CSZ (S : Profinite.{u}) : ring (CSZ S) := sorry
+
+instance alg_CSZ (S : Profinite.{u}) : algebra ℤ (CSZ S) := sorry
+
+def prod_N (I : Type u) : Type u := Π (n : ℕ), I  
+
+def N_order : (ℕ → ℕ → Prop) := λ n m, n ≤ m
+
+def prod_order (I : Type u) : Π (n : ℕ), I → I → Prop := λ n, @well_ordering_rel I 
+
+def lex_order (I : Type u) : (prod_N I) → (prod_N I) → Prop := λ x y, pi.lex N_order (prod_order I) x y 
+
+def sub_prod (I : Type u) : Type u := 
+  {x : prod_N I // (∀ n : ℕ, ordinal.typein (well_ordering_rel) (x n) ≠ 0 → 
+    (ordinal.typein (well_ordering_rel) (x n) > ordinal.typein (well_ordering_rel) (x n.succ))) ∧ 
+    (∃ n : ℕ, ∀ m ≥ n, ordinal.typein (well_ordering_rel) (x m) = 0)}
+
+def subset_prod (I : Type u) : set (prod_N I) := 
+  {x : prod_N I | (∀ n : ℕ, ordinal.typein (well_ordering_rel) (x n) ≠ 0 → 
+    (ordinal.typein (well_ordering_rel) (x n) > ordinal.typein (well_ordering_rel) (x n.succ))) ∧ 
+    (∃ n : ℕ, ∀ m ≥ n, ordinal.typein (well_ordering_rel) (x m) = 0)}
+
+def e (S : Profinite.{u}) : I S → CSZ S := sorry 
+  -- corresponding projection, compose with the map: bool → ℤ, ff mapsto 0, tt mapsto 1
+
+def prod_e (S : Profinite.{u}) : sub_prod (I S) → CSZ S := sorry 
+  -- product of the e i for i ∈ sub_prod (I S), where e 0 = 1
+
+def Eset (S : Profinite.{u}) (I : Type u) : set (prod_N I) := sorry 
+  -- subset satisfying property of not being a linear combination of smaller e_prod's
+
+noncomputable theorem nobelings_thm (S : Profinite.{u}) : CSZ S ≅ free_algebra ℤ (Eset S (I S)) := sorry 
--- src/injective_map.lean
+++ src/injective_map.lean
@@ -3,8 +3,55 @@ import topology.separation
 
 universe u
 
+def I_type (α : Type u) [topological_space α] :=
+  {fI : α → bool // continuous fI}
+
+def map_to_I (α : Type u) [topological_space α] : α → (Π (i : {fI : α → bool // continuous fI}), (function.const {fI : α → bool // continuous fI} bool) i) :=
+  λ a i, ((coe : {fI : α → bool // continuous fI} → (α → bool)) i) a
+
+def map_to_I' (α : Type u) [topological_space α] : α → ({fI : α → bool // continuous fI} → bool) := 
+  λ a i, ((coe : {fI : α → bool // continuous fI} → (α → bool)) i) a
+
+lemma inj_to_prod' (α : Type u) [topological_space α] [totally_separated_space α] : 
+  continuous (map_to_I' α) ∧ function.injective (map_to_I' α) := 
+begin
+  let I := {fI : α → bool // continuous fI},
+  let f : α → (I → bool) := map_to_I' α,
+  have hf : f = map_to_I' α := by triv,
+  split,
+  { refine continuous_pi _, 
+    intros i,
+    cases i, assumption, },
+  { rw ← hf,
+    intros a b hab,
+    let s : set α := {a, b},
+    have h : ∀ fi : α → bool, continuous fi → ∀ x ∈ s, ∀ y ∈ s, fi x = fi y,
+    { intros fi hfi x hx y hy,
+      cases hx, cases hy, work_on_goal 1 { induction hy, induction hx, refl }, work_on_goal 1 { induction hy, induction hx, dsimp at * }, work_on_goal 2 { induction hx, cases hy, work_on_goal 1 { induction hy, dsimp at * }, work_on_goal 2 { induction hy, refl } },
+      { let fi' : I := ⟨fi, hfi⟩,
+        have hab' : f x fi' = f y fi' := by { rw hab },
+        exact hab' },
+      { let fi' : I := ⟨fi, hfi⟩,
+        have hab' : f y fi' = f x fi' := by { rw hab },
+        exact hab'.symm } },
+    by_contra',
+    have h' := exists_clopen_of_totally_separated this,
+    obtain ⟨U, hU, h₁⟩ := h',
+    let fi := set.bool_indicator U,
+    specialize h (set.bool_indicator U) _ a _ b _,
+    { exact (continuous_bool_indicator_iff_clopen U).mpr hU },
+    { exact set.mem_insert a {b} },
+    { exact set.mem_insert_of_mem a rfl },
+    apply h₁.2,
+    cases h₁, cases hU, 
+    rw (set.mem_iff_bool_indicator U a) at h₁_left,
+    rw h₁_left at h,
+    rw set.mem_iff_bool_indicator U b,
+    exact h.symm, },
+end
+
 lemma inj_to_prod (α : Type u) [topological_space α] [totally_separated_space α] : 
-  ∃ (I : Type u), ∃ (f : α → (Π (i : I), (function.const I bool) i)), ∃ hf : continuous f, 
+  ∃ (I : Type u), ∃ (f : α → (Π (i : I), (function.const I bool) i)), continuous f ∧ 
   function.injective f := 
 begin
   let I := {fI : α → bool // continuous fI},
@@ -241,8 +288,6 @@ begin
         exfalso, 
         exact (set.ext_iff.mp hXQ a).mp ⟨h_1, ha⟩ } } },
 end
-
-#check set.ext_iff 
 
 instance totally_separated_of_totally_disconnected_compact_hausdorff (α : Type*) 
   [topological_space α] [hc : compact_space α] [htd :totally_disconnected_space α] [hh : t2_space α] :
